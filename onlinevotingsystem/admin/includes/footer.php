772f279012cc1f5b455fb629715a88af112d0fdd
Desmond Alben <desmondalben@Desmonds-MacBook-Pro.local>
2025-05-27 08:03:28 +1000

    initial commit

---
+++ onlinevotingsystem/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer" style="background-color: #427D9D ">
+    <div class="pull-right hidden-xs "  style="color:white ; font-size: 13px"  >
+      <b>All rights reserved</b>
+    </div  >
+    <b style="color:white ; font-size: 13px"><strong>Copyright &copy; </strong></b>
+</footer>
---
+++ onlinevotingsystem/includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="main-footer" style="background-color: #717A83 ">
+    <div class="container" style="color:white ; font-size: 13px">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <b style="color:white ; font-size: 13px"><strong>Copyright &copy; IT Project SEM 1 2024 </strong></b>
+    </div>
+    <!-- /.container -->
+</footer>
+
